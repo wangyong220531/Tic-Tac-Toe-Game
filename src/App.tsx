98cbcb96ee42d69ff0b603340cbbac6e36406520
王勇 <wy19945372694@163.com>
2023-08-30 11:14:43 +0800

    20230830 11:14 update

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react"
 import "./App.css"
 
 interface SquareProps {
     value: string
+    onSquareClick: () => void
 }
 
 function Square(props: SquareProps) {
-    const { value } = props
-    return <button className="square">{value}</button>
+    const { value, onSquareClick } = props
+
+    return (
+        <button className="square" onClick={onSquareClick}>
+            {value}
+        </button>
+    )
 }
 
 export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null))
+
+    function handleClick(index:number){
+        const nextSquares = squares.slice()
+        nextSquares[index] = "X"
+        setSquares(nextSquares)
+    }
     return (
         <div className="board">
-            <Square value={squares[0]} />
-            <Square value={squares[1]} />
-            <Square value={squares[2]} />
-            <Square value={squares[3]} />
-            <Square value={squares[4]} />
-            <Square value={squares[5]} />
-            <Square value={squares[6]} />
-            <Square value={squares[7]} />
-            <Square value={squares[8]} />
+            {squares.map((e: string, index: number) => {
+                return <Square value={e} onSquareClick={() => handleClick(index)} />
+            })}
         </div>
     )
 }
